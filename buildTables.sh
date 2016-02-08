51210d83a15dc5fa04a6721a392d0dce3fef2790
Alex Gartner <alex@agartner.com>
2016-02-08 17:59:20 -0600

    Added command to create files folder

--- buildTables.sh
+++ buildTables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir static/files
+
 sqlite3 ctf.db 'CREATE TABLE categories ( id INTEGER PRIMARY KEY, name TEXT );'
 sqlite3 ctf.db 'CREATE TABLE tasks (id INTEGER PRIMARY KEY, name TEXT, desc TEXT, file TEXT, flag TEXT, score INT, category INT, FOREIGN KEY(category) REFERENCES categories(id) ON DELETE CASCADE);'
 
